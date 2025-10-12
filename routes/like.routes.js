572d8a136c0d034665853793c5783818b9331006
novaxux <manuelova@outlook.es>
2025-10-12 12:39:07 -0600

    like method changed post->patch

--- routes/like.routes.js
+++ routes/like.routes.js
@@ -4,6 +4,6 @@ import { toggleLike } from "../controllers/like.controller.js";
 const router = Router();
 
 // Toggle like de receta
-router.post("/:recipeId/like", toggleLike);
+router.patch("/:recipeId/like", toggleLike);
 
 export default router;
